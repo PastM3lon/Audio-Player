a30fc12e4d77f6e7935d0d7d1ce12ac29e80e7c7
Omar El Shoura <139502536+PastM3lon@users.noreply.github.com>
2025-10-14 17:06:39 +0300

    Updated PlayerAudio.cpp

--- PlayerAudio.cpp
+++ PlayerAudio.cpp
@@ -1,1 +1,54 @@
+#include "PlayerAudio.h"
+
+PlayerAudio::PlayerAudio()
+{
+    formatManager.registerBasicFormats();
+}
+
+PlayerAudio::~PlayerAudio() {}
+
+void PlayerAudio::prepareToPlay(int samplesPerBlockExpected, double sampleRate)
+{
+    transportSource.prepareToPlay(samplesPerBlockExpected, sampleRate);
+}
+
+void PlayerAudio::getNextAudioBlock(const juce::AudioSourceChannelInfo& bufferToFill)
+{
+    transportSource.getNextAudioBlock(bufferToFill);
+}
+
+void PlayerAudio::releaseResources()
+{
+    transportSource.releaseResources();
+}
+
+void PlayerAudio::loadFile(const juce::File& file)
+{
+    if (file.existsAsFile())
+    {
+        if (auto* reader = formatManager.createReaderFor(file))
+        {
+            transportSource.stop();
+            transportSource.setSource(nullptr);
+            readerSource.reset(new juce::AudioFormatReaderSource(reader, true));
+            transportSource.setSource(readerSource.get(), 0, nullptr, reader->sampleRate);
+        }
+    }
+}
+
+void PlayerAudio::start()
+{
+    transportSource.start();
+}
+
+void PlayerAudio::stop()
+{
+    transportSource.stop();
+    transportSource.setPosition(0.0);
+}
+
+void PlayerAudio::setGain(float gain)
+{
+    transportSource.setGain(gain);
+}
 
