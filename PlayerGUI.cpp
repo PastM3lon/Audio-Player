a7558846877e2de2c2eb8c0b8151d820d83261e1
Omar El Shoura <139502536+PastM3lon@users.noreply.github.com>
2025-10-14 17:07:15 +0300

    Updated PlayerGUI.cpp

--- PlayerGUI.cpp
+++ PlayerGUI.cpp
@@ -1,1 +1,61 @@
+#include "PlayerGUI.h"
+
+PlayerGUI::PlayerGUI(PlayerAudio& audioRef) : audio(audioRef)
+{
+    for (auto* btn : { &loadButton, &playButton, &stopButton })
+    {
+        btn->addListener(this);
+        addAndMakeVisible(btn);
+    }
+
+    volumeSlider.setRange(0.0, 1.0, 0.01);
+    volumeSlider.setValue(0.5);
+    volumeSlider.addListener(this);
+    addAndMakeVisible(volumeSlider);
+}
+
+PlayerGUI::~PlayerGUI() {}
+
+void PlayerGUI::paint(juce::Graphics& g)
+{
+    g.fillAll(juce::Colours::darkgrey);
+}
+
+void PlayerGUI::resized()
+{
+    int y = 20;
+    loadButton.setBounds(20, y, 100, 40);
+    playButton.setBounds(140, y, 60, 40);
+    stopButton.setBounds(220, y, 60, 40);
+    volumeSlider.setBounds(20, 100, getWidth() - 40, 30);
+}
+
+void PlayerGUI::buttonClicked(juce::Button* button)
+{
+    if (button == &loadButton)
+    {
+        fileChooser = std::make_unique<juce::FileChooser>("Select Audio File", juce::File{}, "*.wav;*.mp3");
+        fileChooser->launchAsync(juce::FileBrowserComponent::openMode | juce::FileBrowserComponent::canSelectFiles,
+            [this](const juce::FileChooser& fc)
+            {
+                auto file = fc.getResult();
+                audio.loadFile(file);
+                audio.start();
+            });
+    }
+    else if (button == &playButton)
+    {
+        audio.start();
+    }
+    else if (button == &stopButton)
+    {
+        audio.stop();
+    }
+}
+
+void PlayerGUI::sliderValueChanged(juce::Slider* slider)
+{
+    if (slider == &volumeSlider)
+        audio.setGain((float)slider->getValue());
+}
 
