1a0e46b91aa0380683bc1a435db985ba21f409f4
Omar El Shoura <139502536+PastM3lon@users.noreply.github.com>
2025-10-14 17:07:29 +0300

    Updated PlayerGUI.h

--- PlayerGUI.h
+++ PlayerGUI.h
@@ -1,1 +1,29 @@
+#pragma once
+#include <JuceHeader.h>
+#include "PlayerAudio.h"
+
+class PlayerGUI : public juce::Component,
+    public juce::Button::Listener,
+    public juce::Slider::Listener
+{
+public:
+    PlayerGUI(PlayerAudio& audioRef);
+    ~PlayerGUI() override;
+
+    void paint(juce::Graphics& g) override;
+    void resized() override;
+
+    void buttonClicked(juce::Button* button) override;
+    void sliderValueChanged(juce::Slider* slider) override;
+
+private:
+    PlayerAudio& audio;
+
+    juce::TextButton loadButton{ "Load" };
+    juce::TextButton playButton{ "▶" };
+    juce::TextButton stopButton{ "■" };
+    juce::Slider volumeSlider;
+
+    std::unique_ptr<juce::FileChooser> fileChooser;
+};
 
