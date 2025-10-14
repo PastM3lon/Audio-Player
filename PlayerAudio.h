4b1950479d4b6d7e9689fda543e5fe873ee35053
Omar El Shoura <139502536+PastM3lon@users.noreply.github.com>
2025-10-14 17:06:58 +0300

    Updated PlayerAudio.h

--- PlayerAudio.h
+++ PlayerAudio.h
@@ -1,1 +1,24 @@
+#pragma once
+#include <JuceHeader.h>
+
+class PlayerAudio
+{
+public:
+    PlayerAudio();
+    ~PlayerAudio();
+
+    void prepareToPlay(int samplesPerBlockExpected, double sampleRate);
+    void getNextAudioBlock(const juce::AudioSourceChannelInfo& bufferToFill);
+    void releaseResources();
+
+    void loadFile(const juce::File& file);
+    void start();
+    void stop();
+    void setGain(float gain);
+
+private:
+    juce::AudioFormatManager formatManager;
+    std::unique_ptr<juce::AudioFormatReaderSource> readerSource;
+    juce::AudioTransportSource transportSource;
+};
 
