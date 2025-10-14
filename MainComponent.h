adbcc470dfcdced23c90a6d387079b787c1d5b20
Omar El Shoura <139502536+PastM3lon@users.noreply.github.com>
2025-10-14 17:08:01 +0300

    Updated MainComponent.h to follow Modularization

--- MainComponent.h
+++ MainComponent.h
@@ -1,44 +1,24 @@
 #pragma once
-
 #include <JuceHeader.h>
-#include <vector>
+#include "PlayerAudio.h"
+#include "PlayerGUI.h"
 
-class MainComponent : public juce::AudioAppComponent,
-    public juce::Button::Listener,
-    public juce::Slider::Listener
+class MainComponent : public juce::AudioAppComponent
 {
 public:
     MainComponent();
     ~MainComponent() override;
 
-    // Audio
     void prepareToPlay(int samplesPerBlockExpected, double sampleRate) override;
     void getNextAudioBlock(const juce::AudioSourceChannelInfo& bufferToFill) override;
     void releaseResources() override;
 
-    // GUI
     void paint(juce::Graphics& g) override;
     void resized() override;
 
-    // Event handlers
-    void buttonClicked(juce::Button* button) override;
-    void sliderValueChanged(juce::Slider* slider) override;
-
 private:
-    // Audio
-    juce::AudioFormatManager formatManager;
-    std::unique_ptr<juce::AudioFormatReaderSource> readerSource;
-    juce::AudioTransportSource transportSource;
-
-    // GUI Controls
-    juce::TextButton loadButton{ "Load Files" };
-    juce::TextButton restartButton{ "Restart" };
-    juce::TextButton stopButton{ "Stop" };  
-	juce::TextButton prevButton{ "Test" };
-    juce::Slider volumeSlider;
-
-    //void loadTrack(const juce::File& file);
-    std::unique_ptr<juce::FileChooser> fileChooser;
+    PlayerAudio playerAudio;
+    PlayerGUI playerGUI{ playerAudio };
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(MainComponent)
 };
