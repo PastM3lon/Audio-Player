f3b836be5779b627b2a9079852ee29bc650a656d
siefkhaled <siefkhaled565@gmail.com>
2025-10-22 11:48:27 +0300

    MainComponent.h

--- MainComponent.h
+++ MainComponent.h
@@ -1,24 +1,56 @@
 #pragma once
-#include <JuceHeader.h>
-#include "PlayerAudio.h"
-#include "PlayerGUI.h"
 
-class MainComponent : public juce::AudioAppComponent
+#include <JuceHeader.h>
+#include <vector>
+
+class MainComponent : public juce::AudioAppComponent,
+    public juce::Button::Listener,
+    public juce::Slider::Listener
 {
 public:
     MainComponent();
     ~MainComponent() override;
 
+    // Audio
     void prepareToPlay(int samplesPerBlockExpected, double sampleRate) override;
     void getNextAudioBlock(const juce::AudioSourceChannelInfo& bufferToFill) override;
     void releaseResources() override;
 
+    // GUI
     void paint(juce::Graphics& g) override;
     void resized() override;
 
+    // Event handlers
+    void buttonClicked(juce::Button* button) override;
+    void sliderValueChanged(juce::Slider* slider) override;
+
 private:
-    PlayerAudio playerAudio;
-    PlayerGUI playerGUI{ playerAudio };
+    // Audio
+    juce::AudioFormatManager formatManager;
+    std::unique_ptr<juce::AudioFormatReaderSource> readerSource;
+    juce::AudioTransportSource transportSource;
+
+    // GUI Controls
+    juce::TextButton loadButton{ "Load Files" };
+    juce::TextButton restartButton{ "Restart" };
+    juce::TextButton stopButton{ "Stop" };
+    juce::TextButton loopButton{ "Loop: OFF" }; 
+    juce::Slider volumeSlider;
+
+    bool isLooping = false; 
+
+    std::unique_ptr<juce::FileChooser> fileChooser;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(MainComponent)
+};
+
+
+class mainbutton : public juce::Component
+{
+public:
+    mainbutton();
+    void resized() override;
+
+private:
+    juce::TextButton helloButton;
 };
