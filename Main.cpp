3082408f8ef23a4c42368b200d49867fe7d8a04b
siefkhaled <siefkhaled565@gmail.com>
2025-10-22 22:43:51 +0300

    Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -31,7 +31,7 @@ private:
         {
             setUsingNativeTitleBar(true);
             setContentOwned(new MainComponent(), true); // MainComponent = our UI + logic
-            centreWithSize(800, 400);
+            centreWithSize(1000, 500);
             setVisible(true);
         }
 
@@ -46,3 +46,4 @@ private:
 
 // This macro starts the app
 START_JUCE_APPLICATION(SimpleAudioPlayer)
+
