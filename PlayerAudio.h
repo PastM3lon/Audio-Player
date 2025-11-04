d80fc8b7448590bf338888cfc4a145bff96a8842
alihasanen43 <alihasanen432@gmail.com>
2025-11-04 15:19:17 +0200

    Fix getDuration method placement in PlayerAudio.h

--- PlayerAudio.h
+++ PlayerAudio.h
@@ -26,17 +26,17 @@ public:
     bool isPlaying() const;
     void skipForward(double seconds);
     void skipBackward(double seconds);
-
     juce::String getTitle() const { return title; }
     juce::String getArtist() const { return artist; }
     juce::String getAlbum() const { return album; }
     juce::String getGenre() const { return genre; }
-    double getDuration() const { return duration; }
     juce::String title, artist, album, genre;
+    double getDuration() const { return duration; }
     double duration = 0.0;
     int year = 0, track = 0;
 
 private:
+    juce::Label metaLabel;
     juce::AudioFormatManager formatManager;
     std::unique_ptr<juce::AudioFormatReaderSource> readerSource;
     juce::AudioTransportSource transportSource;
