275dadfb146bec5df2986704e0a2ede38c0d406c
Omar El Shoura <139502536+PastM3lon@users.noreply.github.com>
2025-11-07 13:25:00 +0200

    Remove unused methods and genre member variable

--- PlayerAudio.h
+++ PlayerAudio.h
@@ -6,11 +6,9 @@ class PlayerAudio : public juce::AudioSource
 public:
     PlayerAudio();
     ~PlayerAudio();
-    void mixTracks();
     void prepareToPlay(int samplesPerBlockExpected, double sampleRate) override;
     void getNextAudioBlock(const juce::AudioSourceChannelInfo& bufferToFill) override;
     void releaseResources() override;
-    void runMix(const juce::File& folder1, const juce::File& folder2);
     void loadFile(const juce::File& file);
     void start();
     void stop();
@@ -25,16 +23,15 @@ public:
     bool isPlaying() const;
     void skipForward(double seconds);
     void skipBackward(double seconds);
-    double getCurrentPosition();
-    void setABLooping(bool shouldLoop, double start, double end);
     juce::String getTitle() const { return title; }
     juce::String getArtist() const { return artist; }
     juce::String getAlbum() const { return album; }
-    juce::String getGenre() const { return genre; }
-    juce::String title, artist, album, genre;
+    juce::String title, artist, album;
     double getDuration() const { return duration; }
     double duration = 0.0;
     int year = 0, track = 0;
+    double getCurrentPosition();
+    void setABLooping(bool shouldLoop, double start, double end);
 
 private:
     juce::Label metaLabel;
