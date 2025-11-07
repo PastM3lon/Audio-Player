1c754d34081f3a47999778a30ae0e2f8dcdec1cd
Omar El Shoura <139502536+PastM3lon@users.noreply.github.com>
2025-11-07 13:24:46 +0200

    Remove unused includes and clean up code

--- PlayerAudio.cpp
+++ PlayerAudio.cpp
@@ -1,8 +1,6 @@
 #include "PlayerAudio.h"
 #include <taglib/fileref.h>
 #include <taglib/tag.h>
-#include <cstdint>
-
 
 PlayerAudio::PlayerAudio()
 {
@@ -22,9 +20,9 @@ void PlayerAudio::getNextAudioBlock(const juce::AudioSourceChannelInfo& bufferTo
 {
     resamplingSource->getNextAudioBlock(bufferToFill);
     if (abLooping && loopEnd > loopStart) {
-    double current = transportSource.getCurrentPosition();
-    if (current >= loopEnd)
-        transportSource.setPosition(loopStart);
+        double current = transportSource.getCurrentPosition();
+        if (current >= loopEnd)
+            transportSource.setPosition(loopStart);
     }
 }
 
@@ -43,9 +41,7 @@ void readMetadata(PlayerAudio& player, const juce::File& file)
         player.title = juce::String(juce::CharPointer_UTF8(tag->title().toCString(true)));
         player.artist = juce::String(juce::CharPointer_UTF8(tag->artist().toCString(true)));
         player.album = juce::String(juce::CharPointer_UTF8(tag->album().toCString(true)));
-        player.genre = juce::String(juce::CharPointer_UTF8(tag->genre().toCString(true)));
-        player.year = tag->year();
-        player.track = tag->track();
+        
     }
     else
     {
@@ -53,12 +49,8 @@ void readMetadata(PlayerAudio& player, const juce::File& file)
         player.title = {};
         player.artist = {};
         player.album = {};
-        player.genre = {};
-        player.year = 0;
-        player.track = 0;
     }
 }
-
 
 void PlayerAudio::loadFile(const juce::File& file)
 {
@@ -78,16 +70,12 @@ void PlayerAudio::loadFile(const juce::File& file)
                 DBG("Title: " << title);
                 DBG("Artist: " << artist);
                 DBG(" Album: " << album);
-                DBG("Genre: " << genre);
-                DBG("Year: " << year);
-                DBG(" Track: " << track);
             }
             else
             {
                 DBG(" File: " << file.getFileNameWithoutExtension());
             }
             DBG(" Duration: " << juce::String(duration, 2) + " sec");
-
         }
     }
 }
@@ -169,106 +157,7 @@ void PlayerAudio::skipBackward(double seconds)
     double newPosition = transportSource.getCurrentPosition() - 10;
     if (newPosition < 0.0)
         newPosition = 0.0;
-    transportSource.setPosition(newPosition);	
-}
-void PlayerAudio::mixTracks()
-{
-    auto chooser1 = std::make_shared<juce::FileChooser>(
-        "Select first audio file", juce::File{}, "*.wav;*.mp3");
-
-    chooser1->launchAsync(juce::FileBrowserComponent::canSelectFiles,
-        [this, chooser1](const juce::FileChooser& fc1)
-        {
-            auto file1 = fc1.getResult();
-            if (!file1.existsAsFile()) return;
-
-            auto chooser2 = std::make_shared<juce::FileChooser>(
-                "Select second audio file", juce::File{}, "*.wav;*.mp3");
-
-            chooser2->launchAsync(juce::FileBrowserComponent::canSelectFiles,
-                [this, file1, chooser2](const juce::FileChooser& fc2)
-                {
-                    auto file2 = fc2.getResult();
-                    if (!file2.existsAsFile()) return;
-
-                    runMix(file1, file2);
-                });
-        });
-}
-
-
-void PlayerAudio::runMix(const juce::File& file1, const juce::File& file2)
-{
-    juce::AudioFormatManager formatManager;
-    formatManager.registerBasicFormats();
-
-    auto reader1 = std::unique_ptr<juce::AudioFormatReader>(formatManager.createReaderFor(file1));
-    auto reader2 = std::unique_ptr<juce::AudioFormatReader>(formatManager.createReaderFor(file2));
-
-    if (!reader1 || !reader2)
-    {
-        juce::AlertWindow::showMessageBoxAsync(juce::AlertWindow::WarningIcon,
-            "Mix Error", "Could not read one or both of the selected files.");
-        return;
-    }
-
-    using int64_t = long long;
-
-    int64_t numSamples1 = reader1->lengthInSamples;
-    int64_t numSamples2 = reader2->lengthInSamples;
-    int64_t sampleRate = (int64_t)reader1->sampleRate;
-    int64_t crossfadeLength = (int64_t)(10.0 * sampleRate);
-
-    juce::AudioBuffer<float> buffer1((int)reader1->numChannels, (int)numSamples1);
-    juce::AudioBuffer<float> buffer2((int)reader2->numChannels, (int)numSamples2);
-    reader1->read(&buffer1, 0, (int)numSamples1, 0, true, true);
-    reader2->read(&buffer2, 0, (int)numSamples2, 0, true, true);
-
-    int64_t totalLength = numSamples1 + numSamples2 - crossfadeLength;
-    juce::AudioBuffer<float> mixBuffer(2, (int)totalLength);
-    mixBuffer.clear();
-
-    for (int c = 0; c < 2; ++c)
-    {
-        auto* dest = mixBuffer.getWritePointer(c);
-        auto* src1 = buffer1.getReadPointer(juce::jmin(c, (int)buffer1.getNumChannels() - 1));
-        auto* src2 = buffer2.getReadPointer(juce::jmin(c, (int)buffer2.getNumChannels() - 1));
-
-        for (int i = 0; i < numSamples1 - crossfadeLength; ++i)
-            dest[i] = src1[i];
-
-        for (int i = 0; i < crossfadeLength; ++i)
-        {
-            float fadeOut = 1.0f - (float)i / (float)crossfadeLength;
-            float fadeIn = (float)i / (float)crossfadeLength;
-            dest[i + numSamples1 - crossfadeLength] =
-                src1[i + numSamples1 - crossfadeLength] * fadeOut +
-                src2[i] * fadeIn;
-        }
-
-        for (int i = crossfadeLength; i < numSamples2; ++i)
-            dest[i + numSamples1 - crossfadeLength] = src2[i];
-    }
-
-    auto outputFile = juce::File::getSpecialLocation(juce::File::userDesktopDirectory)
-        .getChildFile("MixedOutput.wav");
-
-    std::unique_ptr<juce::FileOutputStream> output(outputFile.createOutputStream());
-    if (output)
-    {
-        juce::WavAudioFormat wavFormat;
-        if (auto* writer = wavFormat.createWriterFor(output.get(), sampleRate, 2, 16, {}, 0))
-        {
-            output.release();
-            std::unique_ptr<juce::AudioFormatWriter> writerPtr(writer);
-            writerPtr->writeFromAudioSampleBuffer(mixBuffer, 0, mixBuffer.getNumSamples());
-        }
-    }
-
-    juce::AlertWindow::showMessageBoxAsync(
-        juce::AlertWindow::InfoIcon,
-        "Mix Complete",
-        "MixedOutput.wav has been saved to your Desktop!");
+    transportSource.setPosition(newPosition);
 }
 
 double PlayerAudio::getCurrentPosition()
@@ -282,9 +171,3 @@ void PlayerAudio::setABLooping(bool shouldLoop, double start, double end)
     loopStart = start;
     loopEnd = end;
 }
-
-
-
-
-
-
